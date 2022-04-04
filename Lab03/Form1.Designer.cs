ccd07a951211483205f72d4e6af14167d03d3ee3
Tibaran <palacios.tibaran@gmail.com>
2022-04-03 22:43:02 -0500

    lab03

---
+++ Lab03/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace Lab03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServidor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBaseDatos = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chkAutenticacion = new System.Windows.Forms.CheckBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnEstado = new System.Windows.Forms.Button();
+            this.btnDesconectar = new System.Windows.Forms.Button();
+            this.btnPersona = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtServidor
+            // 
+            this.txtServidor.Location = new System.Drawing.Point(27, 38);
+            this.txtServidor.Name = "txtServidor";
+            this.txtServidor.Size = new System.Drawing.Size(136, 20);
+            this.txtServidor.TabIndex = 0;
+            this.txtServidor.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Servidor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Base de Datos";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtBaseDatos
+            // 
+            this.txtBaseDatos.Location = new System.Drawing.Point(27, 95);
+            this.txtBaseDatos.Name = "txtBaseDatos";
+            this.txtBaseDatos.Size = new System.Drawing.Size(136, 20);
+            this.txtBaseDatos.TabIndex = 3;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Enabled = false;
+            this.txtUsuario.Location = new System.Drawing.Point(180, 38);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(134, 20);
+            this.txtUsuario.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Enabled = false;
+            this.txtPassword.Location = new System.Drawing.Point(180, 95);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(134, 20);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(180, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Usuario";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(180, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Contraseña";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // chkAutenticacion
+            // 
+            this.chkAutenticacion.AutoSize = true;
+            this.chkAutenticacion.Checked = true;
+            this.chkAutenticacion.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkAutenticacion.Location = new System.Drawing.Point(27, 133);
+            this.chkAutenticacion.Name = "chkAutenticacion";
+            this.chkAutenticacion.Size = new System.Drawing.Size(91, 17);
+            this.chkAutenticacion.TabIndex = 8;
+            this.chkAutenticacion.Text = "Autenticación";
+            this.chkAutenticacion.UseVisualStyleBackColor = true;
+            this.chkAutenticacion.CheckedChanged += new System.EventHandler(this.chkAutenticacion_CheckedChanged);
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(69, 154);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 9;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnEstado
+            // 
+            this.btnEstado.Enabled = false;
+            this.btnEstado.Location = new System.Drawing.Point(150, 154);
+            this.btnEstado.Name = "btnEstado";
+            this.btnEstado.Size = new System.Drawing.Size(75, 23);
+            this.btnEstado.TabIndex = 10;
+            this.btnEstado.Text = "Estado";
+            this.btnEstado.UseVisualStyleBackColor = true;
+            this.btnEstado.Click += new System.EventHandler(this.btnEstado_Click);
+            // 
+            // btnDesconectar
+            // 
+            this.btnDesconectar.Enabled = false;
+            this.btnDesconectar.Location = new System.Drawing.Point(228, 154);
+            this.btnDesconectar.Name = "btnDesconectar";
+            this.btnDesconectar.Size = new System.Drawing.Size(86, 23);
+            this.btnDesconectar.TabIndex = 11;
+            this.btnDesconectar.Text = "Desconectar";
+            this.btnDesconectar.UseVisualStyleBackColor = true;
+            this.btnDesconectar.Click += new System.EventHandler(this.btnDesconectar_Click);
+            // 
+            // btnPersona
+            // 
+            this.btnPersona.Enabled = false;
+            this.btnPersona.Location = new System.Drawing.Point(150, 189);
+            this.btnPersona.Name = "btnPersona";
+            this.btnPersona.Size = new System.Drawing.Size(75, 23);
+            this.btnPersona.TabIndex = 12;
+            this.btnPersona.Text = "Usuarios";
+            this.btnPersona.UseVisualStyleBackColor = true;
+            this.btnPersona.Click += new System.EventHandler(this.btnPersona_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(357, 224);
+            this.Controls.Add(this.btnPersona);
+            this.Controls.Add(this.btnDesconectar);
+            this.Controls.Add(this.btnEstado);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.chkAutenticacion);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtBaseDatos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtServidor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServidor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBaseDatos;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox chkAutenticacion;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnEstado;
+        private System.Windows.Forms.Button btnDesconectar;
+        private System.Windows.Forms.Button btnPersona;
+    }
+}
+
---
+++ Lab03/frmUsuarios.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Lab03
+{
+    public partial class frmUsuarios : Form
+    {
+        SqlConnection conn;
+        public frmUsuarios(SqlConnection conn)
+        {
+            this.conn = conn;
+            InitializeComponent();
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            if(conn.State == ConnectionState.Open)
+            {
+                String sql = "SELECT * FROM tbl_usuarios";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+                dgvListado.DataSource = dt;
+                dgvListado.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("La conexion esta cerrada");
+            }
+        }
+    }
+}
