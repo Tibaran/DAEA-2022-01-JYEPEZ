52c4ef5e4426ab308b3e0f0f000bf6197e7b797f
Tibaran <palacios.tibaran@gmail.com>
2022-05-22 16:15:14 -0500

    Consulta contains QUESO

---
+++ Lab10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        private static NorthwndDataContext context = new NorthwndDataContext();
+
+        public static void Main()
+        {
+            var query = from p in context.Products
+                        where p.ProductName.Contains("queso")
+                        select p;
+
+            foreach (var prod in query)
+            {
+                Console.WriteLine("ID={0} \t Name={1}", prod.ProductID, prod.ProductName);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
