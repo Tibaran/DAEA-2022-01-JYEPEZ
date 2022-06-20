1b06b1e6a123c25e780e3ce400690f3c5e19354a
Tibaran <palacios.tibaran@gmail.com>
2022-06-19 22:30:04 -0500

    lab14

---
+++ lab14/Controllers/CategoriesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+
+namespace lab14.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private Northwind _entities;
+        // GET: Categories
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
